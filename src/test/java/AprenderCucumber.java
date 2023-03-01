627068a54cf3c8e124eaedfec09df068c4674fc2
Crisley Linhares <prestes40@gmail.com>
2023-02-28 21:24:58 -0400

    Initial commit

---
+++ src/test/java/AprenderCucumber.java
@@ -0,0 +1,73 @@
+import cucumber.api.java.ca.Cal;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+import org.junit.Assert;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class AprenderCucumber {
+
+    @Dado("^que criei o arquivo corretamente$")
+    public void queCrieiOArquivoCorretamente() {
+    }
+
+    @Quando("^executá-lo$")
+    public void executáLo() {
+    }
+
+    @Então("^a especificação deve finalizar com sucesso$")
+    public void aEspecificaçãoDeveFinalizarComSucesso(){
+    }
+
+    private int contador = 0;
+
+    @Dado("^que o valor do contador é (\\d+)$")
+    public void queOValorDoContadorÉ(int arg0) {
+        contador = arg0;
+    }
+
+    @Quando("^eu incrementar em (\\d+)$")
+    public void euIncrementarEm(int arg0) {
+        contador = contador + arg0;
+    }
+
+    @Então("^o valor do contador será (\\d+)$")
+    public void oValorDoContadorSerá(int arg0) {
+        Assert.assertEquals(arg0, contador);
+    }
+
+    Date entrega = new Date();
+
+    @Dado("^que o prazo de entrega é (\\d+)/(\\d+)/(\\d+)$")
+    public void queOPrazoDeEntregaÉ(int dia, int mes, int ano) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, dia);
+        calendar.set(Calendar.MONTH, mes - 1);
+        calendar.set(Calendar.YEAR, ano);
+        entrega = calendar.getTime();
+    }
+
+    @Quando("^a entrega atrasar em (\\d+) (dia|dias|mês|meses)$")
+    public void aEntregaAtrasarEmDias(int periodo, String tempo) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(entrega);
+        if(tempo.equals("dias")){
+            calendar.add(Calendar.DAY_OF_MONTH, periodo);
+        }
+        if(tempo.equals("meses")){
+            calendar.add(Calendar.MONTH, periodo);
+        }
+        entrega = calendar.getTime();
+    }
+
+    @Então("^a entrega será efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
+    public void aEntregaSeráEfetuadaEm(String data) {
+        DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        String dataFormatada = format.format(entrega);
+        Assert.assertEquals(data, dataFormatada);
+    }
+}
---
+++ src/test/java/Runner.java
@@ -0,0 +1,16 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/java"},
+        plugin = "pretty",
+        monochrome = true,
+        snippets = SnippetType.CAMELCASE,
+        dryRun = false,
+        strict = false
+        )
+public class Runner {
+}
