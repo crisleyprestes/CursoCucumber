3f70b385679df41db25685c5072c8af815cbe533
Crisley Linhares <prestes40@gmail.com>
2023-03-06 19:31:32 -0400

    Aula 35 - Gerenciando a massa de dados

--- src/test/java/br/com/linhares/crisley/runners/RunnerTest.java
+++ src/test/java/br/com/linhares/crisley/runners/RunnerTest.java
@@ -3,7 +3,12 @@ package br.com.linhares.crisley.runners;
 import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -17,4 +22,16 @@ import org.junit.runner.RunWith;
         strict = false
         )
 public class RunnerTest {
+
+        @BeforeClass
+        public static void resetarBanco(){
+                WebDriverManager.chromedriver().setup();
+                WebDriver driver = new ChromeDriver();
+                driver.get("https://seubarriga.wcaquino.me/");
+                driver.findElement(By.id("email")).sendKeys("crisley@mail.com");
+                driver.findElement(By.id("senha")).sendKeys("123456");
+                driver.findElement(By.xpath("//*[text()='Entrar']")).click();
+                driver.findElement(By.cssSelector("a[href*='reset']")).click();
+                driver.quit();
+        }
 }
