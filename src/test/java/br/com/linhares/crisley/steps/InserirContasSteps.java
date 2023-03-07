e7163928105f0f169e590fac128bbc5da17763dd
Crisley Linhares <prestes40@gmail.com>
2023-03-07 17:13:04 -0400

    Aula 38 - Gerando screenshots

--- src/test/java/br/com/linhares/crisley/steps/InserirContasSteps.java
+++ src/test/java/br/com/linhares/crisley/steps/InserirContasSteps.java
@@ -1,14 +1,21 @@
 package br.com.linhares.crisley.steps;
 
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
 
 public class InserirContasSteps {
 
@@ -67,7 +74,17 @@ public class InserirContasSteps {
         Assert.assertEquals(mensagem, driver.findElement(By.cssSelector("div[class*='alert']")).getText());
     }
 
-    @After
+    @After(order = 1)
+    public void takeScreenshot(Scenario cenario){
+        File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(file, new File("target/screenshots/" + cenario.getId() + ".jpg"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @After(order = 0)
     public void fecharBrowser(){
         driver.quit();
     }
